201c904c60679bebda83765af1c10b96937abebb
jraduege <git@lvec.org>
2022-05-12 16:24:49 -0700

    Add working moves

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const gameBoard = (() => {
     let started=false;
 
     const spotOpen = (position) => {
-        (boardArray[position] == "U") ? true : false;
+        console.log("Spot open called");
+        let returnValue = (boardArray[position] == "U") ? true : false;
+        console.log(returnValue);
+        return returnValue;
     };
 
     const makeMove = (position, marker) => {
@@ -23,44 +26,87 @@ const gameBoard = (() => {
         return boardArray[position];
     }
 
+    const resetBoard = () => {
+        boardArray = ["U","U","U","U","U","U","U","U","U"];
+    }
+
+    const logBoard = () => {
+        console.log(boardArray);
+    }
+
     return {
         spotOpen,
         makeMove,
         toggleBoard,
         boardOn,
         querySpot,
+        resetBoard,
+        logBoard
     };
 
 })();
 
-const playerFactory = (playerName,marker,firstMove) => {
-    return {playerName, marker, firstMove};
-};
+const players = (() => {
+    let playerArray = [];
+
+    const playerFactory = (playerNumber, playerName,marker,firstMove) => {
+        playerArray.push({playerNumber, playerName, marker, firstMove});
+    };
+
+    const whichPlayer = (marker) => {
+        playerArray.forEach((player) => {
+            if (player.marker==marker) {return player.playerName};
+        });
+    }
+    
+    const logPlayers = () => {
+        console.log(playerArray);
+    }
+
+    return {
+        playerFactory,
+        whichPlayer,
+        logPlayers
+    }
+})();
+
+
 
 const displayControl = (() => {
     let currentMarker;
-    let xPlayer;
-    let oPlayer;
-
-    const places=document.querySelectorAll("place");
-
-    const spots=document.querySelectorAll("spot");
-    spots.forEach(item => {
-        item.addEventListener("click", handleMove);
-    });
+    const status=document.querySelector(".status");
+    const places=document.querySelectorAll(".place");
+    const spots=document.querySelectorAll(".spot");
+ 
+    const setBoard = () => {
+        spots.forEach(item => {
+             item.addEventListener("click", handleMove);
+        });
+    };
 
     const markerToggle = () => {
         currentMarker = (currentMarker == "X") ? "O" : "X";
     };
 
     const handleMove = (e) => {
-        let index=e.target.getAttribute("data-key");
-        if (!gameBoard.boardOn()) { return };
-        if (!gameBoard.spotOpen(index)) { return };
+        let index=e.target.getAttribute("data-index");
+        if (!gameBoard.boardOn()) { 
+            console.log("Board not on")
+            return 
+        };
+        if (!gameBoard.spotOpen(index)) { 
+            console.log("spot not open")
+            return 
+        };
         gameBoard.makeMove(index,currentMarker);
         renderBoard();
-        e.target.classList.toggle("hidden");
         markerToggle();
+
+        let element = e.target.querySelector(".place");
+        if (element) {
+            element.classList.toggle("hidden");
+        }
+
     }
 
     const renderBoard = () => {
@@ -79,7 +125,7 @@ const displayControl = (() => {
             let a = gameBoard.querySpot(combo[0]);
             let b = gameBoard.querySpot(combo[1]);
             let c = gameBoard.querySpot(combo[2]);
-            if ((a==b) && (b==c)) {
+            if (((a==b) && (b==c))&& a!=="U") {
                 endGame("win",a);
                 return;
             }
@@ -90,15 +136,70 @@ const displayControl = (() => {
         for (let i = 0; i < 9; i++) {
             if (gameBoard.querySpot(i) == "U") { return }
           };
-        endGame("tie"); 
+        endGame("tie",""); 
     }
 
     const endGame = (outcome,marker) => {
+        let winner=(marker=="X") ? players.whichPlayer("X") : players.whichPlayer("O");
+        let message="The game is a tie.";
+        gameBoard.toggleBoard();
+        if (outcome=="win") {
+            message=`${winner} wins!`
+        }
+        status.innerHTML = message;
+    }
+
+    const displayRadioValue = (name) => {
+        let ele = document.getElementsByName(name);
+          
+        for(i = 0; i < ele.length; i++) {
+            if(ele[i].checked)
+                return ele[i].value;
+        }
+    }
+
+    const otherMarker = (marker) => {
+        let newMarker = (marker=="X") ? "O" : "X";
+        return newMarker;
+    }
+
+    const isGoingFirst = (player) => {
+        if (player==displayRadioValue("first")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const getsMarker = (first)  => {
+        if (first) {
+            return currentMarker
+        } else {
+            return otherMarker(currentMarker);
+        }
+    }
+
+
+    const startGame = () => {
+        const playerOneName = document.getElementById("player1-name").value;
+        const playerTwoName = document.getElementById("player2-name").value;
+        let playerOneFirst = isGoingFirst("Player One");
+        let playerTwoFirst = isGoingFirst("Player Two");
+
+        currentMarker=displayRadioValue("marker-choice");
+
+        players.playerFactory(1,playerOneName,getsMarker(playerOneFirst),playerOneFirst);
+        players.playerFactory(2,playerTwoName,getsMarker(playerTwoFirst),playerTwoFirst);
+
+        setBoard();
+        gameBoard.toggleBoard();
+
+        console.log("Game started");
 
     }
-    
-    const startGame = () => {
 
+    return {
+        startGame
     }
 
 })();
